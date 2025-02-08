61cd357df4510b7c44e2893729f0f15894062e17
YuuTsunemori0 <valentinblanchard65@gmail.com>
2025-02-08 18:38:54 +0100

    Update budget.js
    révision totale 1

--- scripts/budget.js
+++ scripts/budget.js
@@ -1,12 +1,14 @@
 let budgetTotal = 0;
 let total = 0;
 
+// Fonction pour réinitialiser toutes les dépenses et le budget
 function reinitialiserDepenses() {
     if (confirm("Voulez-vous vraiment réinitialiser toutes les dépenses ?")) {
         expenses = [];
         localStorage.removeItem("expenses");
         total = 0;
         subtotals = { Logement: 0, Transport: 0, Nourriture: 0, Loisirs: 0 };
+
         document.getElementById("total").textContent = "0";
         document.getElementById("categories").innerHTML = "";
     }
